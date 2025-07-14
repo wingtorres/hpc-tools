01bf5ddf8899c9f72e183f416dfb182b8ee0ca8e
Walter Inglis Torres <wtorres@klone-login01.hyak.local>
2025-07-14 11:57:46 -0700

    update suntans job scripts. they work

--- hyak/igw_job.sh
+++ hyak/igw_job.sh
@@ -1,2 +1,16 @@
 export suntansproj="/gscratch/nearshore/wtorres/opt/SUNTANS/examples/oblique_internal_wave" 
-sbatch --ntasks=40 --ntasks-per-node=20 --nodes=2 --output=${suntansproj}/suntans.out --error=${suntansproj}/suntans.err --export=suntansproj=$suntansproj suntans_job.slurm
+
+suntansgrid=$(sbatch --ntasks=20 --ntasks-per-node=20 --nodes=1 --output=${suntansproj}/grid.out --error=${suntansproj}/grid.err --export=suntansproj=$suntansproj --parsable suntans_grid.slurm)
+
+wait ${!}
+sleep 1
+
+#sbatch --ntasks=$nprocs --ntasks-per-node=$taskspernode --nodes=$nodes \
+#--dependency=aftercorr:$suntansgrid \
+#--output=${suntansproj}/suntans.out --error=${suntansproj}/suntans.err --export=coawstproj=$coawstproj coawst_job.slurm
+
+sbatch  --ntasks=20 --ntasks-per-node=20 --nodes=1 \
+	--output=${suntansproj}/suntans.out --error=${suntansproj}/suntans.err \
+	--export=suntansproj=$suntansproj \
+	--dependency=aftercorr:$suntansgrid \
+	suntans_job.slurm
