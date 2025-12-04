1dc3982a95ad70866788b228ab5e3919ce5bac30
Walter Inglis Torres <wtorres@klone-login01.hyak.local>
2025-12-03 17:02:36 -0800

    separate solver and grid+solver jobs

--- hyak/igw_job.sh
+++ hyak/igw_job.sh
@@ -1,16 +1,29 @@
 export suntansproj="/gscratch/nearshore/wtorres/opt/SUNTANS/examples/oblique_internal_wave" 
 
-suntansgrid=$(sbatch --ntasks=20 --ntasks-per-node=20 --nodes=1 --output=${suntansproj}/grid.out --error=${suntansproj}/grid.err --export=suntansproj=$suntansproj --parsable suntans_grid.slurm)
+### GRID & SUNTANS
+
+# Grid build step
+
+#suntansgrid=$(sbatch --ntasks=20 --ntasks-per-node=20 --nodes=1 --output=${suntansproj}/grid.out --error=${suntansproj}/grid.err --export=suntansproj=$suntansproj --parsable suntans_grid.slurm)
+
+# Pause
 
 wait ${!}
 sleep 1
 
-#sbatch --ntasks=$nprocs --ntasks-per-node=$taskspernode --nodes=$nodes \
-#--dependency=aftercorr:$suntansgrid \
-#--output=${suntansproj}/suntans.out --error=${suntansproj}/suntans.err --export=coawstproj=$coawstproj coawst_job.slurm
+# Job submit
+
+#sbatch  --ntasks=20 --ntasks-per-node=20 --nodes=1 \
+#	--output=${suntansproj}/suntans.out --error=${suntansproj}/suntans.err \
+#	--export=suntansproj=$suntansproj \
+#	--dependency=aftercorr:$suntansgrid \
+#	suntans_job.slurm
+
+
+### SUNTANS only
 
 sbatch  --ntasks=20 --ntasks-per-node=20 --nodes=1 \
-	--output=${suntansproj}/suntans.out --error=${suntansproj}/suntans.err \
-	--export=suntansproj=$suntansproj \
-	--dependency=aftercorr:$suntansgrid \
-	suntans_job.slurm
+        --output=${suntansproj}/suntans.out --error=${suntansproj}/suntans.err \
+        --export=suntansproj=$suntansproj \
+        suntans_job.slurm
+
