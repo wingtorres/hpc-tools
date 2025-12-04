7ebcef1ac7ea4caeadd4ea4b809ea33d9b17e4a3
Walter Inglis Torres <wtorres@klone-login01.hyak.local>
2025-12-03 17:03:57 -0800

    updates to be consistent with rip_idealized project.

--- hyak/ripplume_batch.sh
+++ hyak/ripplume_batch.sh
@@ -2,15 +2,15 @@
 
 cppdef="RIP_IDEALIZED"
 export homeproj="/mmfs1/home/wtorres/rip_idealized"
-export ogproj="/Users/wit/COAWST/Projects/rip_idealized"
+export ogproj="Projects/rip_idealized"
 wavnodes=5
 ocnnodes=75
 export nprocs=$(($wavnodes + $ocnnodes))
 export taskspernode=40
 export nodes=$(($nprocs / $taskspernode))
 
-dt=0.125 #time step
-nt=86400 #total time of simulation in seconds
+dt=0.25 #time step
+nt=345600 #total time of simulation in seconds
 ntimes=$(echo "$nt/$dt" | bc)
 nhis=$(echo "3600/$dt" | bc)
 ndefhis=$(echo "86400/$dt" | bc)
@@ -19,19 +19,30 @@ ndefavg=$(echo "3600/$dt" | bc)
 
 #submission loop
 
-#for dTdz in 0.005 0.01 0.02 0.04 0.08 0.16 0.32 0.64 
+for dTdz in 0.0 0.01 0.02 0.04 0.08 0.16 0.32 0.64 
 #for dTdz in 0.01 0.08 
-#do
+do
 
-dTdz=0.02
+#dTdz=0.64
 
 dirname="dTdz_${dTdz}"
-export coawstproj="/gscratch/nearshore/wtorres/results/rip_plumes/${dirname}"
+export coawstproj="/gscratch/nearshore/wtorres/${dirname}"
+echo "$coawstproj"
 
+if [ ! -d "$coawstproj" ]; then #create directory if doesn't exist
+   mkdir ${coawstproj}
+   #cp -a ${homeproj}/. ${coawstproj} #copy all input/grid files to output folder
+   wait ${!}
+fi
+
+rsync -a --exclude=.git ${homeproj}/ ${coawstproj}/
+
+#Modify physical parameters
 
 #sed -i "s#RDRG2 ==.*#RDRG2 ==${rdrg2}d0#" ${coawstproj}/ocean.in
 sed -i "s#dTdz=.*#dTdz=${dTdz}_r8#" ${coawstproj}/ana_initial.h
 
+#Modify computational
 sed -i "s#--nodes=.*#--nodes=${nodes}#" coawst_job.slurm
 sed -i "s#NnodesWAV =.*#NnodesWAV = ${wavnodes}#" ${coawstproj}/coupling.in
 sed -i "s#NnodesOCN =.*#NnodesOCN = ${ocnnodes}#" ${coawstproj}/coupling.in
@@ -53,11 +64,11 @@ sed -i "s#NDEFDIA ==.*#NDEFDIA == $ndefavg#" ${coawstproj}/ocean.in #number time
 sed -i -- "s#${ogproj}#${coawstproj}#g" ${coawstproj}/*.in #find and replace project file path in input files
 sed -i -- "s#${homeproj}#${coawstproj}#g" ${coawstproj}/*.in #find and replace project file path in input files
 
-cp /gscratch/nearshore/wtorres/opt/COAWST/build_coawst.sh ${coawstproj}/coawst.bash #copy build script over
-sed -i "s#MY_PROJECT_DIR=.*#MY_PROJECT_DIR=${coawstproj}#" ${coawstproj}/coawst.bash #Change analytic + header dir
-sed -i "s#COAWST_APPLICATION=.*#COAWST_APPLICATION=${cppdef}#" ${coawstproj}/coawst.bash
+cp /gscratch/nearshore/wtorres/opt/COAWST/build_coawst.sh ${coawstproj}/build_coawst.sh #copy build script over
+sed -i "s#MY_PROJECT_DIR=.*#MY_PROJECT_DIR=${coawstproj}#" ${coawstproj}/build_coawst.sh #Change analytic + header dir
+sed -i "s#COAWST_APPLICATION=.*#COAWST_APPLICATION=${cppdef}#" ${coawstproj}/build_coawst.sh
 sed -i "s#VARNAME =.*#VARNAME = /gscratch/nearshore/wtorres/opt/COAWST/ROMS/External/varinfo.dat#" ${coawstproj}/ocean.in
-chmod +x ${coawstproj}/coawst.bash #fix permissions
+chmod +x ${coawstproj}/build_coawst.sh #fix permissions
 
 coawstcompile=$(sbatch --output=${coawstproj}/compile.out --error=${coawstproj}/compile.err --export=coawstproj=$coawstproj --parsable coawst_compile.slurm)
 
@@ -75,7 +86,7 @@ sbatch --ntasks=$nprocs --ntasks-per-node=$taskspernode --nodes=$nodes \
 wait ${!}
 sleep 1
 
-#done
+done
 
 
 
