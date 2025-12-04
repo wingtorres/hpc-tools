a2355221fd3c41b1695135ddac037b42f8d9c435
Walter Inglis Torres <wtorres@klone-login01.hyak.local>
2025-12-03 17:00:03 -0800

    update for build_coawst.sh script

--- hyak/cape_batch.sh
+++ hyak/cape_batch.sh
@@ -13,7 +13,7 @@ export taskspernode=40
 export nodes=$(($nprocs / $taskspernode))
 
 #Time step and output
-dt=2.0 #time step
+dt=1.0 #time step
 nt=86400 #total time of simulation in seconds
 ntimes=$(echo "$nt/$dt" | bc)
 nhis=$(echo "3600/$dt" | bc)
@@ -34,33 +34,47 @@ ndefavg=$(echo "3600/$dt" | bc)
 #do
 #for amp in 0.0 1.5
 #do
+#for v in 0.0 0.25
+#do
 #for phi in 0.0 45.0
 #do
 
-dSdz=0.15
-Hs=3.0
-direction=300
-amp=1.5
-phi=45.0
+#if (( v == 0.0 )) && (( Hs == 0.0 )); then
+#       continue
+#fi       
 
-dirname="dSdz_${dSdz}_Hs_${Hs}_dir_${direction}_amp_${amp}_phi_${phi}"
-export coawstproj="/gscratch/nearshore/wtorres/output/cape/${dirname}"
+
+dSdz=0.15
+Hs=2.0
+v=0.0
+direction=300
+#amp=0.0
+phi=0.0
+
+#dirname="dSdz_${dSdz}_H_${Hs}_D_${direction}_a_${amp}_f_${phi}"
+#dirname="dSdz_${dSdz}_H_${Hs}_D_${direction}_v_${v}_bathy_multi"
+dirname="dSdz_${dSdz}_H_${Hs}_D_${direction}_v_${v}_bathy_3h"
+export coawstproj="/gscratch/nearshore/wtorres/${dirname}"
 
 #Copy directory over
-if [ ! -d "$coawstproj" ]; then #create directory if doesn't exist
-   mkdir ${coawstproj}
-   mkdir ${coawstproj}/results
-   cp -a ${homeproj}/. ${coawstproj} #copy all input/grid files to output folder
-   wait ${!}
-fi
+#cp -a ${homeproj}/. ${coawstproj} #copy all input/grid files to output folder
+rsync -av ${homeproj}/ ${coawstproj} --exclude=coawstM --exclude=coawstG --exclude=Build* --exclude=grids/
+
+#if [ ! -d "$coawstproj" ]; then #create directory if doesn't exist
+#   mkdir ${coawstproj}
+#   mkdir ${coawstproj}/results
+#   cp -a ${homeproj}/. ${coawstproj} #copy all input/grid files to output folder
+#   wait ${!}
+#fi
+
 
 echo "$coawstproj"
 
 #Modify physical parameters
-echo "modifying physical parameters"
+#echo "modifying physical parameters"
 
 #waves
-sed -i "s#PAR.*#PAR ${Hs} 6.28 ${direction} 10.0#"
+sed -i "s#PAR .*#PAR ${Hs} 6.28 ${direction} 10.0#g" ${coawstproj}/swan.in
 
 #initial stratification
 sed -i "s#dSdz=.*#dSdz=${dSdz}_r8#" ${coawstproj}/ana_initial.h
@@ -70,6 +84,9 @@ sed -i "s#dSdz=.*#dSdz=${dSdz}_r8#" ${coawstproj}/ana_initial.h
 sed -i "s#amp=.*#amp=${amp}_r8#" ${coawstproj}/ana_fsobc.h
 sed -i "s#amp=.*#amp=${amp}_r8#" ${coawstproj}/ana_initial.h
 
+#velocity
+sed -i "s#v0=.*#v0=${v}_r8#" ${coawstproj}/ana_m2obc.h
+
 #Coriolis
 ncap2 -s "f=f*0+2.0*0.0000729*sin(${phi}*3.1415926535897932/180);" -O ${homeproj}/roms_grid.nc ${coawstproj}/roms_grid.nc
 
@@ -77,7 +94,7 @@ ncap2 -s "f=f*0+2.0*0.0000729*sin(${phi}*3.1415926535897932/180);" -O ${homeproj
 #sed -i "s#RDRG2 ==.*#RDRG2 ==${rdrg2}d0#" ${coawstproj}/ocean.in
 
 #Modify computational parameters
-echo "modifying computational parameters..."
+#echo "modifying computational parameters..."
 sed -i "s#--nodes=.*#--nodes=${nodes}#" coawst_job.slurm
 sed -i "s#NnodesWAV =.*#NnodesWAV = ${wavnodes}#" ${coawstproj}/coupling.in
 sed -i "s#NnodesOCN =.*#NnodesOCN = ${ocnnodes}#" ${coawstproj}/coupling.in
@@ -85,14 +102,14 @@ sed -i "s#NtileI ==.*#NtileI == 7#" ${coawstproj}/ocean.in
 sed -i "s#NtileJ ==.*#NtileJ == 5#" ${coawstproj}/ocean.in
 
 #Modify project input file names
-echo "modifying input file names..."
+#echo "modifying input file names..."
 sed -i "s#WAV_name =.*#WAV_name = ${coawstproj}/swan.in#" ${coawstproj}/coupling.in
 sed -i "s#OCN_name =.*#OCN_name = ${coawstproj}/ocean.in#" ${coawstproj}/coupling.in
 sed -i -- "s#${homeproj}#${coawstproj}#g" ${coawstproj}/*.in #find and replace project file path in input files
 sed -i -- "s#${homeproj}#${coawstproj}#g" ${coawstproj}/*.in #find and replace project file path in input files
 
 #Modify timestep & write output
-echo "modifying timestep and output frequency..."
+#echo "modifying timestep and output frequency..."
 sed -i "s#DT ==.*#DT == ${dt}#" ${coawstproj}/ocean.in #change time step
 sed -i "s#NTIMES ==.*#NTIMES == $ntimes#" ${coawstproj}/ocean.in #simulation duration
 sed -i "s#NHIS ==.*#NHIS == $nhis#" ${coawstproj}/ocean.in #history output frequency
@@ -103,7 +120,7 @@ sed -i "s#NDIA ==.*#NDIA == $navg#" ${coawstproj}/ocean.in #diagnostic output fr
 sed -i "s#NDEFDIA ==.*#NDEFDIA == $ndefavg#" ${coawstproj}/ocean.in #number time steps before new .nc file
 
 #Copy build script
-echo "copying build script..."
+#echo "copying build script..."
 cp /gscratch/nearshore/wtorres/opt/COAWST/build_coawst.sh ${coawstproj}/ #copy build script over
 sed -i "s#MY_PROJECT_DIR=.*#MY_PROJECT_DIR=${coawstproj}#" ${coawstproj}/build_coawst.sh #Change analytic + header dir
 sed -i "s#COAWST_APPLICATION=.*#COAWST_APPLICATION=${cppdef}#" ${coawstproj}/build_coawst.sh
@@ -115,14 +132,21 @@ coawstcompile=$(sbatch --output=${coawstproj}/compile.out --error=${coawstproj}/
 wait ${!}
 sleep 1
 
+#Compile
 sbatch --ntasks=$nprocs --ntasks-per-node=$taskspernode --nodes=$nodes \
 --dependency=aftercorr:$coawstcompile \
 --output=${coawstproj}/coawst.out --error=${coawstproj}/coawst.err --export=coawstproj=$coawstproj coawst_job.slurm
 
+#Nocompile
 #sbatch --ntasks=$nprocs --ntasks-per-node=$taskspernode --nodes=$nodes \
 #--output=${coawstproj}/coawst.out --error=${coawstproj}/coawst.err --export=coawstproj=$coawstproj coawst_job.slurm
+
+#sbatch --output=${coawstproj}/coawst.out --error=${coawstproj}/coawst.err --export=coawstproj=$coawstproj coawst_job.slurm
 
 wait ${!}
 sleep 1
 
+#done
+#done
+#done
 #done
